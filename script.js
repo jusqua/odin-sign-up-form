45a9c39a6f93cb62144870c76139544ed69a7052
Ádrian Gama <adrian.gama.se@gmail.com>
2022-06-01 21:26:49 -0300

    feat(ui): finish form

--- script.js
+++ script.js
@@ -2,18 +2,36 @@ function main() {
   const email = document.getElementById('email');
   const password = document.getElementById('password');
   const passwordConfirm = document.getElementById('password-confirm');
+  const submit = document.getElementById('submit');
 
-  console.log(email, password, passwordConfirm);
-}
+  // See regex pattern at https://digitalfortress.tech/tips/top-15-commonly-used-regex
+  const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
+  const passwordRegex = /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,.\/?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/;
 
-function validateEmail(value) {
-  const regex = /[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+/;
-  return regex.test(value);
-}
+  const handleValidation = (e, regex) => {
+    const value = e.target.value;
+    const isValid = regex.test(value);
+    if (isValid)
+      e.target.classList.remove('invalid')
+    else
+      e.target.classList.add('invalid')
+    return isValid;
+  };
 
-function validatePassword(value) {
-  const regex = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9].*[0-9])(?=.*[^a-zA-Z0-9]).{8,}/;
-  return regex.test(value);
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  email.addEventListener('keyup', e => handleValidation(e, emailRegex));
+  email.addEventListener('change', e => handleValidation(e, emailRegex));
+
+  password.addEventListener('keyup', e => handleValidation(e, passwordRegex));
+  password.addEventListener('change', e => handleValidation(e, passwordRegex));
+
+  passwordConfirm.addEventListener('keyup', e => handleValidation(e, passwordRegex) && handleValidation(e, (new RegExp(password.value))));
+  passwordConfirm.addEventListener('change', e => handleValidation(e, passwordRegex) && handleValidation(e, (new RegExp(password.value))));
+
+  submit.addEventListener('submit', e => handleSubmit(e));
 }
 
 main();
